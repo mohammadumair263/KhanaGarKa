12a0ecefe01eba4ca108fe30a0d207ee50b62482
mohammadumair263 <mohammadumair263@gmail.com>
2018-02-26 23:15:22 -0800

    fake chef data

--- FYPFinalKhanaGarKa/Controllers/ChefController.cs
+++ FYPFinalKhanaGarKa/Controllers/ChefController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FYPFinalKhanaGarKa.Models;
 
 namespace FYPFinalKhanaGarKa.Controllers
 {
@@ -10,7 +11,50 @@ namespace FYPFinalKhanaGarKa.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<Chef> chefs = new List<Chef>();
+            chefs.Add(new Chef {
+                ID = 1,
+                Name = "Umair",
+                Address="Daska",
+                ImageUrl = "~/img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 2,
+                Name = "Rashid",
+                Address = "Sialkot",
+                ImageUrl = "~/img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name = "Tayyab",
+                Address = "Addah",
+                ImageUrl = "~/img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name = "Sameer",
+                Address = "Sambriyal",
+                ImageUrl = "~/img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name = "Saqlain",
+                Address = "Sialkot",
+                ImageUrl = "~/img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name = "Husnain",
+                Address = "Sialkot",
+                ImageUrl = "~/img/logo-google.jpg"
+            });
+
+            return View(chefs);
         }
         public IActionResult ChefAcc()
         {
---
+++ FYPFinalKhanaGarKa/Models/Chef.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FYPFinalKhanaGarKa.Models
+{
+    public class Chef
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
