b666f9657a1503cb0ef62d1a2dc06e92f3aeda74
Muhammad Tayyab <tayyabm382@gmail.com>
2018-03-07 23:50:57 +0500

    Load Fake Data in ChefController & Send it to Searchedchef Vew. Change Address Attribute of Chef. Add search Bar in index page.

--- FYPFinalKhanaGarKa/Controllers/ChefController.cs
+++ FYPFinalKhanaGarKa/Controllers/ChefController.cs
@@ -15,48 +15,119 @@ namespace FYPFinalKhanaGarKa.Controllers
             chefs.Add(new Chef {
                 ID = 1,
                 Name = "Umair",
-                Address="Daska",
+                City="Sialkot",
+                Area="Daska",
                 ImageUrl = "img/logo-google.jpg"
             });
             chefs.Add(new Chef
             {
                 ID = 2,
                 Name = "Rashid",
-                Address = "Sialkot",
+                City = "Sialkot",
+                Area = "Daska",
                 ImageUrl = "img/logo-google.jpg"
             });
             chefs.Add(new Chef
             {
                 ID = 1,
                 Name = "Tayyab",
-                Address = "Addah",
+                City = "Sialkot",
+                Area = "Daska",
                 ImageUrl = "img/logo-google.jpg"
             });
             chefs.Add(new Chef
             {
                 ID = 1,
-                Name = "Sameer",
-                Address = "Sambriyal",
+                City = "Sialkot",
+                Area = "Daska",
                 ImageUrl = "img/logo-google.jpg"
             });
             chefs.Add(new Chef
             {
                 ID = 1,
                 Name = "Saqlain",
-                Address = "Sialkot",
+                City = "Sialkot",
+                Area = "Daska",
                 ImageUrl = "img/logo-google.jpg"
             });
             chefs.Add(new Chef
             {
                 ID = 1,
                 Name = "Husnain",
-                Address = "Sialkot",
+                City = "Sialkot",
+                Area = "Daska",
                 ImageUrl = "img/logo-google.jpg"
             });
 
             return View(chefs);
         }
         public IActionResult ChefAcc()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult searchChef()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult searchChef(string City, string Area)
+        {
+            List<Chef> chefs = new List<Chef>();
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name = "Umair",
+                City = "Sialkot",
+                Area = "Daska",
+                ImageUrl = "img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 2,
+                Name = "Rashid",
+                City = "Sialkot",
+                Area = "Sialkot",
+                ImageUrl = "img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name = "Tayyab",
+                City = "Sialkot",
+                Area = "Daska",
+                ImageUrl = "img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name="Sameer",
+                City = "Sialkot",
+                Area = "Sambrial",
+                ImageUrl = "img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name = "Saqlain",
+                City = "Sialkot",
+                Area = "Daska",
+                ImageUrl = "img/logo-google.jpg"
+            });
+            chefs.Add(new Chef
+            {
+                ID = 1,
+                Name = "Husnain",
+                City = "Sialkot",
+                Area = "Pasrur",
+                ImageUrl = "img/logo-google.jpg"
+            });
+
+            IList<Chef> searchedchefs = chefs.Where(m => m.City.Contains(City) && m.Area.Contains(Area)).ToList<Chef>();
+
+            return View("searchedchefs",searchedchefs);
+        }
+        public IActionResult searchedchefs()
         {
             return View();
         }
